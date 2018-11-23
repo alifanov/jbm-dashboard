635a5b43aef19e4bbcb061e2c08efc82e38f4496
Alexander Lifanov <lifanov.a.v@gmail.com>
2018-11-23 15:09:13 +0300

    Updates

--- precache-manifest.bb9d79ac0cb3172243eaf0e3273f3ab6.js
+++ precache-manifest.bb9d79ac0cb3172243eaf0e3273f3ab6.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/jbm-dashboard/static/js/runtime~main.e74d19ab.js"
   },
   {
-    "revision": "61ea6bfe876a48084729",
-    "url": "/jbm-dashboard/static/js/main.61ea6bfe.chunk.js"
+    "revision": "eb56c93b3f811eb0d453",
+    "url": "/jbm-dashboard/static/js/main.eb56c93b.chunk.js"
   },
   {
     "revision": "99016492aadc0d42aac6",
     "url": "/jbm-dashboard/static/js/1.99016492.chunk.js"
   },
   {
-    "revision": "61ea6bfe876a48084729",
+    "revision": "eb56c93b3f811eb0d453",
     "url": "/jbm-dashboard/static/css/main.0e4badd2.chunk.css"
   },
   {
-    "revision": "de56253228daa9a83e80abc4f748369e",
+    "revision": "00ff6c7bee5a80a864d20c23adbc0bf5",
     "url": "/jbm-dashboard/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/jbm-dashboard/precache-manifest.bb1ebf51b3a3e4c32142069737b8de30.js"
+  "/jbm-dashboard/precache-manifest.bb9d79ac0cb3172243eaf0e3273f3ab6.js"
 );
 
 workbox.clientsClaim();
