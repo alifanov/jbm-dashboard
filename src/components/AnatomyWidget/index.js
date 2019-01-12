e915398a942f1d8470c2db384ee262374e4911e2
alifanov <lifanov.a.v@gmail.com>
2019-01-12 21:37:05 +0300

    Add point for anatomy widget

--- src/components/AnatomyWidget/index.js
+++ src/components/AnatomyWidget/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 
 import {FaExpandArrowsAlt} from "react-icons/fa";
+import {
+  Tooltip,
+} from 'react-tippy';
 
 import Widget from "../Widget";
 
 import "./index.css";
+import 'react-tippy/dist/tippy.css';
+
 
 const proxyUrl = "https://cors-anywhere.herokuapp.com/";
 
@@ -31,8 +36,30 @@ export default class AnatomyWidget extends Widget {
         <span className="dragging">
           <FaExpandArrowsAlt />
         </span>
+        <div className='zones'>
+          <Tooltip
+          title='Molar'
+          position={'right'}
+          >
+            <div className='zone' style={{
+              top: '60px',
+              left: '295px',
+            }}/>
+          </Tooltip>
+          <Tooltip
+          title='Hair'
+          position={'right'}
+          >
+            <div className='zone'
+                 style={{
+                   top: '15px',
+                   left: '305px'
+                 }}
+            />
+          </Tooltip>
+        </div>
         <div style={{textAlign: 'center'}}>
-          <img src='/human-body.png' height={600} />
+          <img src='/human-body-bold.png' height={600} />
         </div>
       </div>
     );
